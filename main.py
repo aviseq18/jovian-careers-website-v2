c220312bac73c90005fac6de31f9e6b400da2237
aviseq18 <abhishekraj1896@gmail.com>
2024-12-10 01:45:12 +0100

    desined html page using bootstrap

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title':"Data Analyst",
+    "location":"Bengaluru",
+    "Salary": "INR 1-5 Lacs"
+  },
+  {
+    'id':2,
+    'title':"Cloud Engineer",
+    "location":"Delhi"
+  },
+  {
+    'id':3,
+    'title':"Business Analyst",
+    "location":"Mumbai",
+    "Salary": "INR 6-10 Lacs"
+  }
+]
 
 @app.route("/")
 def hello():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS, company_name="Jovian")
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
