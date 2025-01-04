991ab7ef505f5095934471794c5cfed4113db045
aviseq18 <abhishekraj1896@gmail.com>
2025-01-04 01:54:12 +0100

    job specific page

--- database.py
+++ database.py
@@ -18,5 +18,18 @@ def load_jobs_from_db():
       jobs.append(row._asdict())
     # print(jobs)
     return jobs 
+  
+def load_job_from_db(id):
+  configure()
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :jobId"),
+      {"jobId":id}
+      )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
 
 # load_jobs_from_db()    
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 """
@@ -35,6 +35,13 @@ def hello():
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(load_jobs_from_db())
+
+@app.route("/job/<id>")
+def show_job(id):
+  jobIdDetails = load_job_from_db(id)
+  if not jobIdDetails:
+    return "Not Found", 404
+  return render_template("jobpage.html", job=jobIdDetails, company_name = "Jovian")
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
